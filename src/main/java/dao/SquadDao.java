623e9eae0f9ac5b38988b9dbf6b23eed20d399c6
DruSadeMumba <drusademumba@gmail.com>
2022-05-01 23:13:16 +0300

    Add behaviours to squad interface

--- src/main/java/dao/SquadDao.java
+++ src/main/java/dao/SquadDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Squad;
+
+import java.util.List;
+
 public interface SquadDao {
+    List<Squad> getAllSquads();
+
+    //CREATE
+    void addSquad (Squad squad);
+
+    //READ
+    Squad findById(int id);
+    List<Squad> getAllHeroesBySquads(int squadId);
+
+    //UPDATE
+    void update(int id, String name, String cause, int maxSize);
+
+    //DELETE
+    void deleteById(int id);
+    void clearAllSquads();
+
 }
--- src/test/java/dao/SqlSquadDaoTest.java
+++ src/test/java/dao/SqlSquadDaoTest.java
@@ -7,11 +7,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SqlSquadDaoTest {
 
-    @BeforeEach
-    void setUp() {
-    }
 
-    @AfterEach
-    void tearDown() {
-    }
 }
