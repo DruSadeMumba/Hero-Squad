d68dec8f798071fb386a7c3508d23969c9e0e48a
DruSadeMumba <drusademumba@gmail.com>
2022-05-01 22:09:22 +0300

    Add methods to Heroes.java

--- src/main/java/models/Heroes.java
+++ src/main/java/models/Heroes.java
@@ -4,5 +4,71 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Heroes {
+    private String name;
+    private int age;
+    private String powers;
+    private String weakness;
+    private boolean completes;
+    private LocalDateTime joinedAt;
+    private int id;
+    private int squadId;
 
+    public Heroes(String name, int age, String powers, String weakness, int categoryId){
+        this.name = name;
+        this.age = age;
+        this.powers =powers;
+        this.weakness = weakness;
+        this.completes = false;
+        this.joinedAt = LocalDateTime.now();
+        this.squadId = squadId;
+    }
+    public int getSquadId() {
+        return squadId;
+    }
+    public void setSquadIdId(int squadId) {this.squadId = squadId;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Heroes)) return false;
+        Heroes heroes = (Heroes) o;
+        return getcompletes() == heroes.getcompletes() && getId() == heroes.getId() && Objects.equals(getPowers(), heroes.getPowers());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge(), getPowers(), getWeakness(),  getcompletes(), getId());
+    }
+    public void setPowers(String powers) {
+        this.powers = powers;
+    }
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCompletes(boolean completes) {
+        this.completes = completes;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPowers() {
+        return powers;
+    }
+    public String getWeakness() {
+        return weakness;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {return name;}
+    public boolean getcompletes(){return this.completes;}
+    public LocalDateTime getJoinedAt() {return joinedAt;}
+    public int getId() {
+        return id;
+    }
 }
