6be4d1e1fa89f61e0494cca4e351958c111cf139
DruSadeMumba <drusademumba@gmail.com>
2022-05-02 11:36:55 +0300

    Add villains squad form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import dao.SqlHeroesDao;
+import dao.SqlSquadDao;
+import models.Squad;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -11,16 +15,29 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         port(2345);
+
+        String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        SqlSquadDao squadDao = new SqlSquadDao(sql2o);
+        SqlHeroesDao heroesDao = new SqlHeroesDao(sql2o);
+
+        Map<String, Object> model = new HashMap<String, Object>();
         get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
         get("/heroes", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "heroes.hbs");
         }, new HandlebarsTemplateEngine());
         get("/villains", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "villains.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/villainssquad/new", (req, res) -> { //working
+            return new ModelAndView(model, "villainsquad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/villainssquad", (req, res) -> { //new
+            squadDao.addSquad(new Squad(req.queryParams("name"), req.queryParams("purpose"), Integer.parseInt(req.queryParams("maxSize"))));
+            model.put("squads", squadDao.getAllSquads());
+            model.put("heroes", heroesDao.getAll());
             return new ModelAndView(model, "villains.hbs");
         }, new HandlebarsTemplateEngine());
     }
--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,13 +1,13 @@
 SET MODE PostgreSQL;
-
+DROP TABLE heroes;
 CREATE TABLE IF NOT EXISTS heroes (
   id int PRIMARY KEY auto_increment,
   name VARCHAR,
   age INT,
   powers VARCHAR,
   weakness VARCHAR,
-  squadsId INTEGER
-  completes BOOLEAN,
+  squadId INTEGER,
+  completes BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS squads (
